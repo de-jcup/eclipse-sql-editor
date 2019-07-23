dd9549b2b2126bf2924d1fac68aeef69c85cb482
de-jcup <albert.tregnaghi@gmail.com>
2019-07-23 13:46:05 +0200

    Deployment 1.0.0 #7
    - upgraded missing parts to 1.0.0
    - added missing copyright information

--- deploy.sh
+++ deploy.sh
@@ -16,4 +16,4 @@ if [ -z "$BINTRAY_API_KEY" ]; then
 	echo "bintray api key not set"
 	show_help_and_exit;
 fi
-./pushToBintray.sh ${BINTRAY_USER} ${BINTRAY_API_KEY} de-jcup sqleditor update-site current ./sqleditor-updatesite/
+./pushToBintray.sh ${BINTRAY_USER} ${BINTRAY_API_KEY} de-jcup sqleditor update-site 1.0.0 ./sqleditor-updatesite/
